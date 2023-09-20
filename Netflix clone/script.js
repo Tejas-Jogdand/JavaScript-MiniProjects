fb0393007765bd5e918ec8fc0b5493fe82f6079f
unknown <tejasjogdand37@gmail.com>
2023-09-20 20:48:23 +0530

    Netflix clone and practice folder

---
+++ Netflix clone/script.js
@@ -0,0 +1,65 @@
+let movies = [ 
+    
+    {
+        name:"RRR",
+        ratting:8.7,
+        poster:"https://m.media-amazon.com/images/M/MV5BODUwNDNjYzctODUxNy00ZTA2LWIyYTEtMDc5Y2E5ZjBmNTMzXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg",
+        description:"Action movie"
+    },
+    {
+        name:"Infinity war",
+        ratting:9.5,
+        poster:"https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_.jpg",
+        description:"marvel"
+    },
+    {
+        name:"https://m.media-amazon.com/images/M/MV5BZDEyN2NhMjgtMjdhNi00MmNlLWE5YTgtZGE4MzNjMTRlMGEwXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_FMjpg_UX1000_.jpg",
+        ratting:8.7,
+        poster:"",
+        description:"dramer"
+    },
+    {
+        name:"Ant man",
+        ratting:8.9,
+        poster:"https://m.media-amazon.com/images/M/MV5BMjM2NTQ5Mzc2M15BMl5BanBnXkFtZTgwNTcxMDI2NTE@._V1_FMjpg_UX1000_.jpg",
+        description:"entertanment"
+    },
+    {
+        name:"Endgame",
+        ratting:8.6,
+        poster:"https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
+        description:"marvel movie"
+    },
+    {
+        name:"Iron man",
+        ratting:8.7,
+        poster:"https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_FMjpg_UX1000_.jpg",
+        description:"action movie"
+    },
+    {
+        name:"Gardian of gallexy",
+        ratting:9.7,
+        poster:"https://m.media-amazon.com/images/M/MV5BNDIzMTk4NDYtMjg5OS00ZGI0LWJhZDYtMzdmZGY1YWU5ZGNkXkEyXkFqcGdeQXVyMTI5NzUyMTIz._V1_.jpg",
+        description:"duplicate"
+    },
+    {
+        name:"Gardian of gallexy 2",
+        ratting:9.5,
+        poster:"https://assets.gadgets360cdn.com/pricee/assets/product/202203/GOG-2-poster_1648409965.jpg",
+        description:"Drama, Action"
+    },
+    {
+        name:"Fast and Furious",
+        ratting:9.5,
+        poster:"https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/Fast_and_Furious_Poster.jpg/220px-Fast_and_Furious_Poster.jpg",
+        description:"Racing"
+    },
+    {
+        name:"Death note",
+        ratting:9.0,
+        poster:"https://upload.wikimedia.org/wikipedia/en/7/72/Death_Note_Characters.jpg",
+        description:"Anime"
+    }
+
+]
+console.log(document);
---
+++ Practice folder/script.js
@@ -0,0 +1,10 @@
+function printevent(){
+    // console.log(document.getElementById("one").innerHTML="<h1>Hi Everyone</h1>");
+    // console.log(document.getElementById("one").innerText="Hi Everyone");
+    // console.log(document.getElementById("one").style.height="300px");
+    // console.log(document.getElementById("one").style.backgroundColor="pink");
+    console.log(document.getElementById("one").style.marginLeft="300px");
+    console.log(document.getElementById("one").style.transform="rotateZ(360deg)");
+}
+
+
